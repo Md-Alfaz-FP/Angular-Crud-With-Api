e7d955656225436e2fc9e7c6177adca5a0d66334
Md Alfaz <126404954+Md-Alfaz-FP@users.noreply.github.com>
2023-05-08 11:00:18 +0600

    Add files via upload

---
+++ Backend/CardsAPI/Data/CardsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CardsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardsAPI.Data
+{
+    public class CardsDbContext:DbContext
+    {
+        public CardsDbContext(DbContextOptions<CardsDbContext> options) : base(options) 
+        {
+        }
+        public DbSet<Card> Cards { get; set; }
+    }
+}
---
+++ Backend/CardsAPI/Program.cs
@@ -0,0 +1,41 @@
+using CardsAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CardsDbContext>(Options=> 
+Options.UseSqlServer(builder.Configuration.GetConnectionString("API")));
+
+
+builder.Services.AddCors((setup) =>
+{
+  setup.AddPolicy("default", (Options) =>
+  {
+    Options.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+  });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("default");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
